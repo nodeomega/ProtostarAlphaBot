3d0c1ab9ccc4500838d0e4416c1d11884c0604ff
Jon Russell <jrussell@nodeomega.com>
2018-01-08 02:09:34 -0800

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=protostaralphabot-Web-Deploy.pubxml -p:Password=B9CXoeb112bddiD8sbYBFMKkuYlTY5cAJoztxGqFkc5CvkCXv4a8A7uDvo71
+
